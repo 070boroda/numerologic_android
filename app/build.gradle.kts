b43afea3e81e44ffb9d80ed820b1870ea8dc66cb
aliaksandr.zelianko <aliaksandr.zelianko@rdigital.by>
2023-10-19 08:33:01 +0300

    [NA-011] -  subscription

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,9 +48,13 @@ android {
 }
 
 dependencies {
+    implementation("com.android.billingclient:billing-ktx:6.0.1")
+
     implementation ("com.yandex.android:mobileads:6.0.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
